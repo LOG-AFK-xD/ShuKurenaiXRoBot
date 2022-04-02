8a50370b5b1b860523263b250ffdeee6fc1eecb1
/Oꜰꜰʟɪɴᴇ ‌[Aꜰᴋ] <93505687+LOG-AFK-xD@users.noreply.github.com>
2022-04-02 22:06:52 +0530

    Update upload.py

--- ShuKurenaiXRoBot/modules/upload.py
+++ ShuKurenaiXRoBot/modules/upload.py
@@ -159,7 +159,7 @@ async def up(event):
                 event.chat.id,
                 dosya,
                 force_document=True,
-                caption=f"Uploaded By *ShuKurenaiXRoBot*",
+                caption=f"Uploaded By *Kiara_ro_bot*",
             )
         except Exception as e:
             traceback.print_exc()
